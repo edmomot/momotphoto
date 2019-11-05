8bc2c55b9e5e3ca293e93de87204a760fa448c37
Ed Momot <ed.momot@gmail.com>
2019-11-05 16:25:57 -0500

    Added basic vue project with routing

---
+++ src/web/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to='/'>Home</router-link>
+      <router-link to='/About'>About</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/web/src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import About from './components/About.vue';
+
+export default [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About }
+];
