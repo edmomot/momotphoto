e413a255f17c3676dae655f8882deed1109db84b
Ed Momot <ed.momot@gmail.com>
2019-11-07 13:33:49 -0500

    Modified api to return URLs instead of local file paths

--- api/AlbumController.js
+++ api/AlbumController.js
@@ -3,17 +3,23 @@ const directoryReader = require('./DirectoryReader');
 const jsonFileReader = require('./JsonFileReader');
 const directoryTree = require('directory-tree');
 
-const albumRootPath = '../albums';
+const AlbumReader = require('./AlbumReader');
+
+const albumRootPath = './public/albums';
 
 const albumController = {
     GetAllAlbums: (request, response) => {
-        AlbumDataBuilder.init(directoryReader, jsonFileReader);
+        // AlbumDataBuilder.init(directoryReader, jsonFileReader);
+        //
+        // let dirTree = directoryTree(albumRootPath);
+        //
+        // AlbumDataBuilder.build(dirTree)
+        //     .then(x => response.send(x),
+        //     error => response.status(500).send("Error building directory " + albumRootPath + ': ' + error));
 
-        let dirTree = directoryTree(albumRootPath);
-
-        AlbumDataBuilder.build(dirTree)
+        AlbumReader.ReadAllAlbums()
             .then(x => response.send(x),
-            error => response.status(500).send("Error building directory " + albumRootPath + ': ' + error));
+            error => response.status(500).send('Error building directory : ' + error));
     }
 };
 
---
+++ api/AlbumReader.js
@@ -0,0 +1,49 @@
+const AlbumDataBuilder = require('./AlbumDataBuilder');
+const directoryReader = require('./DirectoryReader');
+const jsonFileReader = require('./JsonFileReader');
+const directoryTree = require('directory-tree');
+
+const albumRootPath = './public/albums';
+
+function ReadAllAlbums() {
+    AlbumDataBuilder.init(directoryReader, jsonFileReader);
+    let dirTree = directoryTree(albumRootPath);
+    return AlbumDataBuilder.build(dirTree).then(x => transformRecursively(x, transformAlbum));
+}
+
+function transformRecursively(rootNode, transformNode) {
+    if (rootNode.albums) {
+        return {
+            ...transformNode(rootNode),
+            albums: rootNode.albums.map(x => transformRecursively(x, transformNode))
+        }
+    }
+
+    return transformNode(rootNode);
+}
+
+function transformImage(image) {
+    return {
+        name: image.name,
+        url: convertPathToUrl(image.path)
+    }
+}
+
+function transformAlbum(album) {
+    return {
+        name: album.name,
+        url: (album.path ? convertPathToUrl(album.path) : undefined),
+        images: album.images ? album.images.map(transformImage) : undefined
+    }
+}
+
+function convertPathToUrl(path) {
+    return path
+        .split('\\')
+        .filter(x => x !== '..')
+        .filter(x => x !== 'public')
+        .map(encodeURI)
+        .join('/');
+}
+
+module.exports = { ReadAllAlbums };
