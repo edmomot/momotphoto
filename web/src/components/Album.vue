e9c9d807bf951a729e9c5413f9f40cbbfbfd9153
Ed Momot <ed.momot@gmail.com>
2019-11-14 11:03:29 -0500

    Smarter resizing, invisible images only on loading and initial repack

--- web/src/components/Album.vue
+++ web/src/components/Album.vue
@@ -4,7 +4,7 @@
 
         <h2>Images</h2>
 
-        <div :class='[ imageContainerClass, { repacking: repacking }]'>
+        <div :class="[imageContainerClass, { 'loading-images': loadingImages}]">
             <img v-for='image in album.images' v-bind:key='image.name' :src='image.thumbnail' :alt='image.name' />
         </div>
     </div>
@@ -20,9 +20,9 @@ export default {
     },
     data: () => { return {
         imageContainerClass: 'image-container',
+        loadingImages: true,
         bricksInstance: null,
-        repacking: true,
-        waitingOnResizeStop: false,
+        repacking: true
     }},
     computed: {
         imageContainerSelector: function () {
@@ -41,38 +41,17 @@ export default {
             position: false
         });
 
-        this.debouncedResize = this.debounce(this.resize, 500, () => this.repacking = true);
+        this.bricksInstance.resize(true);
 
         ImagesLoaded(this.imageContainerSelector, () => {
             this.bricksInstance.pack();
-            window.addEventListener('resize', this.debouncedResize);
+            this.loadingImages = false;
         });
     },
     methods: {
-        resize: function () {
-            this.bricksInstance.pack();
-            setTimeout(() => this.repacking = false, 250);
-        },
         range: function (start, end, step) {
             return [...Array(Math.floor((end - start) / step)).keys()].map(i => i * step + start);
-        },
-        debounce: function (func, wait, immediate) {
-            let timeout;
-            return function() {
-                let context = this, args = arguments;
-                let later = function() {
-                    timeout = null;
-                    if (!immediate) func.apply(context, args);
-                };
-                let callNow = immediate && !timeout;
-                clearTimeout(timeout);
-                timeout = setTimeout(later, wait);
-                if (callNow) func.apply(context, args);
-            }
         }
-    },
-    beforeDestroy: function () {
-        window.removeEventListener('resize', this.debouncedResize);
     }
 }
 </script>
@@ -85,10 +64,9 @@ export default {
 .image-container {
     margin: auto;
     opacity: 1;
-    transition: opacity 0.25s;
 }
 
-.image-container.repacking {
-    opacity: 0.5;
+.image-container.loading-images {
+    opacity: 0;
 }
 </style>
