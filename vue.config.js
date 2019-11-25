4fc1bd95940f6fe11cd0202510daeb5e6c958e19
Ed Momot <ed.momot@gmail.com>
2019-11-25 14:34:21 -0500

    Purge unused CSS on build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const glob = require('glob-all');
+const path = require('path');
+
 module.exports = {
-  "devServer": {},
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+    "devServer": {},
+    "transpileDependencies": [
+        "vuetify"
+    ],
+    configureWebpack: {
+        // Merged into the final Webpack config
+        plugins: [
+            new PurgecssPlugin({
+                paths: glob.sync([
+                    path.join(__dirname, './src/index.html'),
+                    path.join(__dirname, './**/*.vue'),
+                    path.join(__dirname, './src/**/*.js')
+                ])
+            })
+        ]
+    }
+};
