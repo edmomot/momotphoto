33c79ad186ccf56c95c58e2ecf29fc893091ec80
Ed Momot <ed.momot@gmail.com>
2019-11-25 14:58:55 -0500

    Modified config for purgecss to work with vuetify

--- vue.config.js
+++ vue.config.js
@@ -12,9 +12,10 @@ module.exports = {
         plugins: [
             new PurgecssPlugin({
                 paths: glob.sync([
-                    path.join(__dirname, './src/index.html'),
+                    path.join(__dirname, './public/index.html'),
                     path.join(__dirname, './**/*.vue'),
-                    path.join(__dirname, './src/**/*.js')
+                    path.join(__dirname, './src/**/*.js'),
+                    path.join(__dirname, './node_modules/vuetify/src/**/*.ts'),
                 ])
             })
         ]
