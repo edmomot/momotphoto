4da10dd50aae66d474d972a9318a70e2466b7df8
Ed Momot <ed.momot@gmail.com>
2019-12-02 14:09:11 -0500

    Added basic tiered loading, displaying only a handful of images on initial load

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,17 +1,26 @@
 <template>
     <div class='album'>
-        <div :class="[imageContainerClass, { 'loading-images': loadingImages}]">
-            <img v-for='(image, index) in album.images'
+        <div :class="[ imageContainerClass, { 'loading-images': loadingImagesAboveTheFold }]">
+            <img v-for='(image, index) in imagesAboveTheFold'
                  v-bind:key='image.name'
                  :src='image.thumbnail'
                  :alt='image.name'
+                 class="image-above-the-fold"
                  @click="showFullSizeImage(image, index)"/>
+            <template v-if="doneLoadingImagesAboveTheFold">
+                <img v-for='(image, index) in imagesBelowTheFold'
+                     v-bind:key='image.name'
+                     :src='image.thumbnail'
+                     :alt='image.name'
+                     class="image-below-the-fold"
+                     @click="showFullSizeImage(image, index)"/>
+            </template>
         </div>
 
         <full-screen-image
                 :isOpen="fullSizeImageOpen"
                 :options="fullSizeImageOptions"
-                :items="imagesWithSize"
+                :items="fullScreenImages"
                 @close="hideFullSizeImage"/>
     </div>
 </template>
@@ -27,9 +36,9 @@ export default {
     },
     data: () => { return {
         imageContainerClass: 'image-container',
-        loadingImages: true,
+        loadingImagesAboveTheFold: true,
+        doneLoadingImagesAboveTheFold: false,
         bricksInstance: null,
-        repacking: true,
         fullSizeImageOpen: false,
         fullSizeImageOptions: {
             index: 0
@@ -39,31 +48,40 @@ export default {
         imageContainerSelector: function () {
             return '.' + this.imageContainerClass;
         },
-        imagesWithSize: function() {
-            return this.album.images.map(x => ({ w: x.width, h: x.height, src: x.url, title: x.name }))
+        imagesAboveTheFold: function () {
+            return this.album.images.slice(0, 8);
+        },
+        imagesBelowTheFold: function() {
+            return this.album.images.slice(8);
+        },
+        fullScreenImages: function () {
+            return this.album.images.map(this.mapImageToFullScreenImageData);
         }
     },
     mounted: function () {
-        const gutter = 2;
-        this.bricksInstance = Bricks({
-            container: this.imageContainerSelector,
-            packed: 'data-packed',
-            sizes: [
-                { columns: 1, gutter },
-                { mq: '400px', columns: 2, gutter },
-                { mq: '600px', columns: 3, gutter },
-                { mq: '800px', columns: 4, gutter },
-                ...this.range(1200, 1920 * 2, 200).map(size => ({ mq: size + 'px', columns: Math.floor((size - 250) / 200), gutter }))
-            ],
-            position: false
-        });
-
-        this.bricksInstance.resize(true);
-        this.packImagesAfterLoad();
+    },
+    beforeRouteLeave: function(to, from, next) {
+        this.loadingImagesAboveTheFold = true;
+        this.doneLoadingImagesAboveTheFold = false;
+        next();
     },
     beforeRouteEnter: function(to, from, next) {
         next(vm => {
-            vm.loadingImages = true;
+            const gutter = 2;
+            vm.bricksInstance = Bricks({
+                container: vm.imageContainerSelector,
+                packed: 'data-packed',
+                sizes: [
+                    { columns: 1, gutter },
+                    { mq: '400px', columns: 2, gutter },
+                    { mq: '600px', columns: 3, gutter },
+                    { mq: '800px', columns: 4, gutter },
+                    ...vm.range(1200, 1920 * 2, 200).map(size => ({ mq: size + 'px', columns: Math.floor((size - 250) / 200), gutter }))
+                ],
+                position: false
+            });
+
+            vm.bricksInstance.resize(true);
             vm.packImagesAfterLoad();
         });
     },
@@ -78,11 +96,29 @@ export default {
         hideFullSizeImage: function () {
             this.fullSizeImageOpen = false;
         },
-        packImagesAfterLoad: function() {
-            ImagesLoaded(this.imageContainerSelector, () => {
-                this.bricksInstance.pack();
-                this.loadingImages = false;
+        packImagesAfterLoad: function () {
+            this.$nextTick(() => {
+                ImagesLoaded(this.imageContainerSelector + ' > img.image-above-the-fold', () => {
+                    this.bricksInstance.pack();
+                    this.loadingImagesAboveTheFold = false;
+                    this.doneLoadingImagesAboveTheFold = true;
+                    this.loadImagesBelowTheFold();
+                });
             });
+        },
+        loadImagesBelowTheFold: function () {
+            const loadRemainingImages = () => {
+                ImagesLoaded(this.imageContainerSelector + ' > img.image-below-the-fold', () => {
+                    this.bricksInstance.update();
+                });
+            };
+
+            //setTimeout(loadRemainingImages, 2000);
+
+            this.$nextTick(loadRemainingImages);
+        },
+        mapImageToFullScreenImageData: function (image) {
+            return { w: image.width, h: image.height, src: image.url, title: image.name }
         }
     },
     components: {
@@ -98,16 +134,19 @@ export default {
 
 .image-container {
     margin: auto;
-    opacity: 1;
-    transition: opacity 200ms;
-}
-
-.image-container.loading-images {
-    opacity: 0;
-    transition: opacity 0s; /* transition to full opacity immediately */
 }
 
 img {
     border-radius: 4px;
+
+    /* loading */
+    opacity: 0;
+    transition: opacity 0s; /* transition to full opacity immediately */
+}
+
+img[data-packed] {
+    /* loaded */
+    opacity: 1;
+    transition: opacity 200ms;
 }
 </style>
